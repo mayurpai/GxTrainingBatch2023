3b204ae4991eba3efa460874e50fa51a0df29581
mayurpai <mayur5pai@gmail.com>
2023-11-20 15:25:47 +0530

    Exports

---
+++ Mayur Pai/react-training/src/Sample.js
@@ -0,0 +1,13 @@
+export function SampleOne() {
+  return <h1>Sample One</h1>;
+}
+
+export function SampleTwo() {
+  return <h1>Sample Two</h1>;
+}
+
+export function SampleThree() {
+  return <h1>Sample Three</h1>;
+}
+
+export default SampleOne;
--- Mayur Pai/react-training/src/index.js
+++ Mayur Pai/react-training/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SampleOne, { SampleThree as Three, default as SampleTwo } from "./Sample";
+// import { SampleTwo } from "./Sample";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SampleOne />
+    <SampleTwo />
+    <Three/>
+    {/* {SampleOne()} */}
   </React.StrictMode>
 );
 
